6969ed120d60abcbaab1b273a3c827b61cee3e0d
Irving Didier <bider88@gmail.com>
2020-11-22 17:09:25 -0600

    avatars

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,28 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://www.revistaclase.mx/sites/default/files/field/image/amber-heard_0.jpg'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/placeholder.gif'),
+          image: NetworkImage('https://los40es00.epimg.net/los40/imagenes/2017/04/05/cinetv/1491384105_586437_1491384320_noticia_normal.jpg'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
